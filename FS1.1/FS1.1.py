76d05e28f69e68d691aaff59f5c9ffb6d7a111ce
NattePappelo <axel.appelo@gmail.com>
2024-07-26 22:21:14 +0300

    Create FS1.1.py

---
+++ FS1.1/FS1.1.py
@@ -0,0 +1,16 @@
+print("Får simulator 1.1")
+print("")
+
+najs = True
+
+while najs == True:
+    print("Säg Bää")
+    ba = input()
+    if ba == "Bä" or ba == "Bää" or ba == "bää" or ba == "bä":
+        print("bra bräkt")
+        print("")
+    else:
+        najs = False
+        print("")
+        print("Game over!!!")
+q = input()
